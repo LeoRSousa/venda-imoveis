70c23513dd8bab371e9caf05a232e416a4402b81
Leonardo Sousa <38815972+LeoRSousa@users.noreply.github.com>
2021-07-28 20:38:42 -0300

    Sidebar V1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'venda-imoveis';
+  title = 'Sistema para Controle de Vendas de Imóveis';
 }
