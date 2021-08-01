69ff520a5707a6a11c59206215a348eadb66e3b8
Leonardo Sousa <38815972+LeoRSousa@users.noreply.github.com>
2021-08-01 20:40:36 -0300

    Atualização na home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -22,11 +22,29 @@ export class HomeComponent implements OnInit {
 
   imoveisLista: Imovel[] = [
     new Imovel(1, this.imoveisTipo[0], this.imagem, 'João Souza', 500000, 'Casa - Rua do Metrô, Iguape, Ilhéus, BA', '2020-12-29'),
+    new Imovel(2, this.imoveisTipo[1], this.imagem, 'Ana Dias', 300000, 'Casa - Rua dos Carijós, Centro, Belo Horizonte, MG', '2020-12-29'),
   ];
-
+  
   tipo = '';
   visualizar() {
     console.log(this.tipo);
+  }
+
+  editar(elemento: number) {
+    //Abrir um alerta com campos de imput para alterar o imovel
+    Swal.fire({
+      title: 'Editar',
+      text: `${elemento}`
+    });
+  }
+
+  deletar(elemento: number) {
+    for (let index = 0; index < this.imoveisLista.length; index++) {
+      if(this.imoveisLista[index].codigo == elemento){
+        this.imoveisLista.splice(index, 1);
+        return;  
+      }
+    };
   }
 
   public showMessage() {
